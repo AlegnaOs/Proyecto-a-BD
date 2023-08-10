41ab4a88451d7afcfa766cd4be779dd643c58aaf
Angela Moreno <angela.moreno.cubillos@gmail.com>
2023-08-09 20:15:48 -0500

    ok

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const pg = require('pg');
 
+//const Pool = pg.Pool;
+
 const pool = new pg.Pool({
     host: 'localhost',
     port: 5432,
@@ -8,11 +10,27 @@ const pool = new pg.Pool({
     password: 'root',
 });
 
-pool.query("SELECT * from actor", (error, results) => {
-    if(error){
-        console.error('Ocurio un error');
+pool.query("SELECT * from actor WHERE first_name='Penelope'", (error, results) => {
+    if(error) {
+        console.error('Ocurrió un error');
         console.error(error);
         return;
     }
-    console.log(results.rowCount);
+    console.log(results.rows);
+});
+
+
+const persona = {
+    name: 'Mali',
+    apellido: 'Becerril'
+}
+
+
+pool.query("INSERT INTO actor(first_name, last_name) VALUES ($1, $2)", [persona.name, persona.apellido], (error, results) => {
+    if(error) {
+        console.error('Ocurrió un error');
+        console.error(error);
+        return;
+    }
+    console.log(JSON.stringify(results));
 });
